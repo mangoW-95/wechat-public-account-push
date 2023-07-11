f7fd4870e610b3ebddefae56480eebfb9b529321
mangoW-95 <69748877+mangoW-95@users.noreply.github.com>
2023-07-11 22:46:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -154,7 +154,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆2',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oCjxW6qHjLTtwKUy4JXx4DVbnbJg',
+      id: 'oCjxW6mkM_wSvi7TwNS4d4bKh87U',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'v4EJWkMcEJr8wJxUF4EQ0gvVv1oqxq3UBw11G79Qpd0',
       // 所在省份
