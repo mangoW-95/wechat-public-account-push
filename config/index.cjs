8913808b52e0a4a0ae44e63275a97cb654ec9851
mangoW-95 <69748877+mangoW-95@users.noreply.github.com>
2023-07-11 20:52:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,32 +8,32 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx8b381dadbc2fb9ad',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'fab92cc0cec0fb885a1e8c059ddec965',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '北京',
+  CITY: '北京',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oCjxW6jGKOKrVBNQH1qs42b409Wg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'GV73bBjHqQlfc4EkM6nWTQ2Q00h5tRKSySBZppu-rhk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝2', year: '1996', date: '09-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝贝', year: '1995', date: '07-27',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
