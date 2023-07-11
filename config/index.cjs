a5d69e185c9898fcd6f282ea4acfb8ff68ac2bcb
mangoW-95 <69748877+mangoW-95@users.noreply.github.com>
2023-07-11 21:26:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCjxW6jGKOKrVBNQH1qs42b409Wg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'jrEjjcFrnBk6zmIwxtd-84EMWtx95wdVdqVIV8ocMyY',
+      useTemplateId: 'c2DJXnOXSSeoEIA7u8Iiw8-rnhSJQsK5iz7GZjiU89A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-27',
       festivals: [
