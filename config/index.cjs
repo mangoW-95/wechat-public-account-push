5c151229981365eab4c50f286ceda831717d97db
mangoW-95 <69748877+mangoW-95@users.noreply.github.com>
2023-07-17 23:16:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,7 +59,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCjxW6jGKOKrVBNQH1qs42b409Wg',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'qjNpdbvOsKaIstOHtawa6NsqymklTELJAqDNXId_Veo',
+      useTemplateId: 'HcTRMzqJYEjB6RCwS1HE781dztysOIx2Uk1P5wRBMjA',
       // 所在省份
       province: '北京市',
       // 所在城市
@@ -154,9 +154,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆2',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oCjxW6mkM_wSvi7TwNS4d4bKh87U',
+      id: '测试oCjxW6mkM_wSvi7TwNS4d4bKh87U',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'qjNpdbvOsKaIstOHtawa6NsqymklTELJAqDNXId_Veo',
+      useTemplateId: 'HcTRMzqJYEjB6RCwS1HE781dztysOIx2Uk1P5wRBMjA',
       // 所在省份
       province: '北京',
       // 所在城市
