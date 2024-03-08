1d768f49c120b402c7fda5fbfc51da2a0aa3e76f
mangoW-95 <69748877+mangoW-95@users.noreply.github.com>
2024-03-08 12:48:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,7 +59,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCjxW6jGKOKrVBNQH1qs42b409Wg',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'XsEdC5fJDC0LBbUmjPXoP6Ve6R2TiGeHK8XJ2FpiTG8',
+      useTemplateId: 'xxmJYXhS6DzNIECTHX5fBDz6EJ_fsdAH00HO7fjp5dU',
       // 所在省份
       province: '北京市',
       // 所在城市
@@ -69,7 +69,7 @@ const USER_CONFIG = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'http://154.8.197.229/pic/03-07.jpg',
+      openUrl: 'http://154.8.197.229/pic/03-08.png',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -156,7 +156,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCjxW6mkM_wSvi7TwNS4d4bKh87U',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'XsEdC5fJDC0LBbUmjPXoP6Ve6R2TiGeHK8XJ2FpiTG8',
+      useTemplateId: 'xxmJYXhS6DzNIECTHX5fBDz6EJ_fsdAH00HO7fjp5dU',
       // 所在省份
       province: '北京',
       // 所在城市
@@ -166,7 +166,7 @@ const USER_CONFIG = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: 'http://154.8.197.229/pic/03-07.jpg',
+      openUrl: 'http://154.8.197.229/pic/03-08.png',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
