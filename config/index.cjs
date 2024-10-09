4716ad756e416d80ac774e315ca0556757e14405
mangoW-95 <69748877+mangoW-95@users.noreply.github.com>
2024-10-09 23:29:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -68,7 +68,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCjxW6jGKOKrVBNQH1qs42b409Wg',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'klSodMy8UuUJRP1eq8iUvK9Eu8mV3K5uLXg9Sw7x5YY',
+      useTemplateId: 'gSyf7b1NdeTy_RzmyKg3enFXkbGvfkPbYEfmphdHjF4',
       // 所在省份
       province: '北京市',
       // 所在城市
@@ -167,7 +167,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCjxW6mkM_wSvi7TwNS4d4bKh87U-test',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'klSodMy8UuUJRP1eq8iUvK9Eu8mV3K5uLXg9Sw7x5YY',
+      useTemplateId: 'gSyf7b1NdeTy_RzmyKg3enFXkbGvfkPbYEfmphdHjF4',
       // 所在省份
       province: '北京',
       // 所在城市
